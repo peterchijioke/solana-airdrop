f85612204009c7d9e92c38e70f718a4402df07fe
Chukwu (Chijioke)  Peter <peterchijioke1@gmail.com>
2024-08-09 13:23:18 +0100

    feat: airdrop

--- index.js
+++ index.js
@@ -7,3 +7,33 @@ const {
 } = require("@solana/web3.js");
 
 const wallet = new Keypair();
+const publicKey = new PublicKey(wallet._keypair.publicKey);
+const secretKey = wallet._keypair.secretKey;
+
+const getWalletBalance = async () => {
+  try {
+    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+    const walletBalance = await connection.getBalance(publicKey);
+    console.log(walletBalance, "====Wallet balance");
+  } catch (error) {
+    console.error(error?.message);
+  }
+};
+
+const airDropSol = async () => {
+  try {
+    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+    const fromAirDropSignature = connection.requestAirdrop(
+      publicKey,
+      2 * LAMPORTS_PER_SOL
+    );
+    await connection.confirmTransaction(fromAirDropSignature);
+  } catch (error) {
+    console.error(error?.message);
+  }
+};
+const main = async () => {
+  await getWalletBalance();
+  await airDropSol();
+};
+main();
